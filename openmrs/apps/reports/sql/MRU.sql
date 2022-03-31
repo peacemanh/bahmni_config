4a4df1edbd7234852153ed5c0fd178ccbe48b22e
saidmurad <saidmurad50@gmail.com>
2022-03-31 14:20:07 +0300

    Added new MRU report with visit

---
+++ openmrs/apps/reports/sql/MRU.sql
@@ -0,0 +1,17 @@
+SELECT DISTINCT
+  pi.identifier                                                 AS "MRN",
+  concat(pn.given_name, " ", ifnull(pn.middle_name, " "), " ", ifnull(pn.family_name, " "))       AS "Patient Name",
+  GROUP_CONCAT(vt.name)                                         AS "Visit Type",
+  u.username                                                    AS "MRU Officer"
+
+FROM visit v
+  JOIN visit_type vt ON v.visit_type_id = vt.visit_type_id
+  JOIN person p ON p.person_id = v.patient_id
+  JOIN users u ON u.user_id = v.creator
+  JOIN patient_identifier pi ON p.person_id = pi.patient_id
+  JOIN person_name pn ON pn.person_id = p.person_id
+
+WHERE
+    cast(v.date_created as date) BETWEEN '#startDate#' and '#endDate#'
+GROUP BY
+  pi.identifier
