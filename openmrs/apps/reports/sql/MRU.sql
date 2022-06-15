e761aaec37cd295de0eb0e91d598f274ee6cb9b4
saidmurad <saidmurad50@gmail.com>
2022-06-15 10:40:26 +0300

    report modifications

--- openmrs/apps/reports/sql/MRU.sql
+++ openmrs/apps/reports/sql/MRU.sql
@@ -1,17 +1,24 @@
 SELECT DISTINCT
-  pi.identifier                                                 AS "MRN",
-  concat(pn.given_name, " ", ifnull(pn.middle_name, " "), " ", ifnull(pn.family_name, " "))       AS "Patient Name",
-  GROUP_CONCAT(vt.name)                                         AS "Visit Type",
-  u.username                                                    AS "MRU Officer"
+  vt.name                                         AS "Service Unit",
+  count(v.patient_id)                             AS "Number of patient"
 
 FROM visit v
   JOIN visit_type vt ON v.visit_type_id = vt.visit_type_id
-  JOIN person p ON p.person_id = v.patient_id
-  JOIN users u ON u.user_id = v.creator
-  JOIN patient_identifier pi ON p.person_id = pi.patient_id AND pi.identifier REGEXP ('^[0-9]+$')
-  JOIN person_name pn ON pn.person_id = p.person_id
 
 WHERE
     cast(v.date_created as date) BETWEEN '#startDate#' and '#endDate#'
 GROUP BY
-  pi.identifier
+  vt.name
+
+UNION ALL
+select "Total", sum(s.patients)
+ from(
+  select
+  count(v.patient_id) AS "patients"
+  FROM visit v
+  JOIN visit_type vt ON v.visit_type_id = vt.visit_type_id
+  WHERE
+  cast(v.date_created as date) BETWEEN '#startDate#' and '#endDate#'
+  GROUP BY
+  vt.name
+) as s
