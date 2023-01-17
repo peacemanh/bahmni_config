ac267b188ec18451cef1a6070c25ba173607bf0c
Selamsew Tesfaye <selamsewtesfaye10@gmail.com>
2023-01-17 18:29:42 +0000

    Added CBHI Information for attribute conditioning

--- openmrs/apps/registration/attributesConditions.js
+++ openmrs/apps/registration/attributesConditions.js
@@ -6,17 +6,24 @@ Bahmni.Registration.AttributesConditions.rules = {
         };
         if(patient["PaymentMethod"] && patient["PaymentMethod"].value && patient["PaymentMethod"].value == "Insurance"){
             returnValues.show.push("insuranceInformation");
-            returnValues.hide.push("creditInformation");
+            returnValues.hide.push("creditInformation", "CBHIInformation");
            
         }
         else if(patient["PaymentMethod"] && patient["PaymentMethod"].value && patient["PaymentMethod"].value == "Credit"){
             returnValues.show.push("creditInformation");
-            returnValues.hide.push("insuranceInformation");
+            returnValues.hide.push("insuranceInformation", "CBHIInformation");
            
         }
+        else if(patient["PaymentMethod"] && patient["PaymentMethod"].value && patient["PaymentMethod"].value == "CBHI"){
+            returnValues.show.push("CBHIInformation");
+            returnValues.hide.push("insuranceInformation", "creditInformation");
+           
+        }
+
         else {
             returnValues.hide.push("creditInformation");
             returnValues.hide.push("insuranceInformation");
+            returnValues.hide.push("CBHIInformation");
         }
         return returnValues;
     }
